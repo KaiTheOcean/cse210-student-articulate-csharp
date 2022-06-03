7b74e0d61306ca4c53ab19fb39780a6a9a8887ee
kai <kaidizhang@byui.edu>
2022-06-03 12:28:30 -0600

    final change

--- Unit04/Game/Casting/Artifact.cs
+++ Unit04/Game/Casting/Artifact.cs
@@ -11,22 +11,28 @@ namespace Unit04.Game.Casting
         /// The responsibility of an Artifact is to provide a message about itself.
         /// </para>
         /// </summary>
-
-
+    public class Artifact : Actor
+    {
+        private string messgae = "";
     // 2) Create the class constructor. Use the following method comment.
         
         /// <summary>
         /// Constructs a new instance of Artifact.
         /// </summary>
-       
+        public Artifact()
+        {
 
+        }
     // 3) Create the GetMessage() method. Use the following method comment.
         
         /// <summary>
         /// Gets the artifact's message.
         /// </summary>
         /// <returns>The message as a string.</returns>
-        
+        public string GetMessage()
+        {
+            return messgae;
+        }
 
     // 4) Create the SetMessage(string message) method. Use the following method comment.
         
@@ -34,5 +40,9 @@ namespace Unit04.Game.Casting
         /// Sets the artifact's message to the given value.
         /// </summary>
         /// <param name="message">The given message.</param>
-    
+        public void SetMessage(string message)
+        {
+            this.messgae = message;
+        }
+    }   
 }
